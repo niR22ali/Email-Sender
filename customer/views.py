26d16c916ed937f582568c6a2548c4e861303aa6
Nirali <nirali0222@gmail.com>
2022-07-12 15:25:21 +0530

    initial commit

---
+++ customer/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib import messages
+
+# Create your views here.
+
+def email(request):
+    return render(request,'customer/email.html')
+
+def email_info(request):
+    myto = request.POST['to']
+    mysubject = request.POST['subject']
+    mymessage = request.POST['message']
+
+
+    #email
+    send_mail(mysubject,mymessage,settings.EMAIL_HOST_USER,[myto],fail_silently=False)
+    messages.success(request,'Email sent successfully!')
+    return redirect('/customer/email/')
+
