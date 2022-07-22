85edf615e9aee5da7c0291a95ab6ac9305848cb4
Nirali Tak <nirali.tak2201@gmail.com>
2022-07-22 15:18:40 +0530

    Update settings.py

--- emailsender/settings.py
+++ emailsender/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1uq$_c=1h_ei%z2ive$m)$vxnqyi$nc-&w51wxly#tpyh(=gr^'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
